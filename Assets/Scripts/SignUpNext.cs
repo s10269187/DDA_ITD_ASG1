34b8789c6399ad413188212d0628e6164bdbd00a
s10269187 <s10269187@connect.np.edu.sg>
2025-12-03 16:05:55 +0800

    change scene to next

---
+++ Assets/Scripts/SignUpNext.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SignUpNext : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("Playground");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
