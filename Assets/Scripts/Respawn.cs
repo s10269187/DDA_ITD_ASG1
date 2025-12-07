70389c20af40152a36b3022e0acb357b9d97887b
s10269187 <s10269187@connect.np.edu.sg>
2025-12-08 00:49:26 +0800

    Prefabs can respawn and collect more than once

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject itemPrefab;     // prefab to respawn
+    public float respawnTime = 10f;
+    public float delayTime = 10f;
+    public int counter = 0;
+    public int maxCunter = 2;
+
+    void Start() 
+    {   
+        gameObject.SetActive(false);              
+        Invoke(nameof(RespawnItem), delayTime);
+        Debug.Log("Spawn timer started");
+    }
+
+    public void CollectItem()
+    {
+        if(counter < maxCunter)
+        {        
+            // Hide or destroy collected object
+            gameObject.SetActive(false);
+
+            // Start respawn timer
+            Invoke(nameof(RespawnItem), respawnTime);
+            counter++;
+        }
+        else{
+            Debug.Log("No more Spawn");
+        }
+    }
+
+    void RespawnItem()
+    {
+        gameObject.SetActive(true);
+    }
+}
