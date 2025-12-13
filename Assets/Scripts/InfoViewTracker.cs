230a7db4148a908bdf3304c230a4142b519a3805
Leong Ming Hui <S10267664@connect.np.edu.sg>
2025-12-14 03:30:52 +0800

    testing changing to end screen
    supposed to work when player has viewed all 3 info and close them but its not detecting hawflake close button

---
+++ Assets/Scripts/InfoViewTracker.cs
@@ -0,0 +1,62 @@
+/// <summary>
+/// InfoViewTracker.cs
+/// This script handles the viewing of the infographic for each snack
+/// once player has viewed all three infographic, they will be
+/// redirected to the end screen
+/// </summary>
+/// <author> Leong Ming Hui </author>
+/// <date> 13/12/2025 </date>
+/// <StudentID> S10267664J </StudentID>
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoViewTracker : MonoBehaviour
+{
+    public static InfoViewTracker Instance;
+
+    
+    public GameObject biscuitInfoPanel;
+    public GameObject bangkitInfoPanel;
+    public GameObject hawflakesInfoPanel;
+
+    bool viewedBiscuit;
+    bool viewedBangkit;
+    bool viewedHawflakes;
+
+    void Awake()
+    {
+        Instance = this;  // allow static access
+    }
+
+
+    public void CloseBiscuitInfo()
+    {
+        Debug.Log("Biscuit Info closed!");
+        viewedBiscuit = true;
+        CheckIfPlayerIsDone();
+    }
+
+    public void CloseBangkitInfo()
+    {
+        Debug.Log("Bangkit Info closed!");
+        viewedBangkit = true;
+        CheckIfPlayerIsDone();
+    }
+
+    public void CloseHawflakesInfo()
+    {
+        Debug.Log("Hawflakes Info closed!");
+        viewedHawflakes = true;
+        CheckIfPlayerIsDone();
+    }
+
+    private void CheckIfPlayerIsDone() {
+        Debug.Log($"CheckIfPlayerIsDone called. Biscuit={viewedBiscuit}, Bangkit={viewedBangkit}, Hawflakes={viewedHawflakes}");
+
+        if (viewedBiscuit && viewedBangkit && viewedHawflakes) {
+            Debug.Log("ALL VIEWED, LOADING END SCREEN!");
+            SceneManager.LoadScene("EndScreen");
+        }
+    }
+
+}   
