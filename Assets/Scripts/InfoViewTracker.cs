c4b7fa4dc754fb3a5c5c24d64c4bd2bc7df27f24
Leong Ming Hui <S10267664@connect.np.edu.sg>
2025-12-14 12:05:33 +0800

    xml and line comments for InfoViewTracker Script
    this script handles the viewing of infographic for each snack + going to end screen

--- Assets/Scripts/InfoViewTracker.cs
+++ Assets/Scripts/InfoViewTracker.cs
@@ -12,30 +12,39 @@ using UnityEngine.SceneManagement;
 
 public class InfoViewTracker : MonoBehaviour
 {
+
+    /// Singleton instance for global access
     public static InfoViewTracker Instance;
 
     
+    /// Info panels for each snack
     public GameObject biscuitInfoPanel;
     public GameObject bangkitInfoPanel;
     public GameObject hawflakesInfoPanel;
 
+    
+    /// Flags to track if each infographic has been viewed
     bool viewedBiscuit;
     bool viewedBangkit;
     bool viewedHawflakes;
 
+    
+    /// Initializes the singleton instance
     void Awake()
     {
         Instance = this;  // allow static access
     }
 
-
+    
+    /// Closes the biscuit info panel and checks if all infographics have been viewed
     public void CloseBiscuitInfo()
     {
         Debug.Log("Biscuit Info closed!");
-        viewedBiscuit = true;
-        CheckIfPlayerIsDone();
+        viewedBiscuit = true;   // mark as viewed
+        CheckIfPlayerIsDone();  // check if all viewed
     }
 
+    /// Closes the bangkit info panel and checks if all infographics have been viewed
     public void CloseBangkitInfo()
     {
         Debug.Log("Bangkit Info closed!");
@@ -43,6 +52,7 @@ public class InfoViewTracker : MonoBehaviour
         CheckIfPlayerIsDone();
     }
 
+    /// Closes the hawflakes info panel and checks if all infographics have been viewed
     public void CloseHawflakesInfo()
     {
         Debug.Log("Hawflakes Info closed!");
@@ -50,13 +60,14 @@ public class InfoViewTracker : MonoBehaviour
         CheckIfPlayerIsDone();
     }
 
+    /// Checks if all infographics have been viewed and loads the end screen if so
     private void CheckIfPlayerIsDone() {
         Debug.Log($"CheckIfPlayerIsDone called. Biscuit={viewedBiscuit}, Bangkit={viewedBangkit}, Hawflakes={viewedHawflakes}");
 
         if (viewedBiscuit && viewedBangkit)
         {
-            Debug.Log("ALL VIEWED, LOADING END SCREEN!");
-            SceneManager.LoadScene("EndScreen");
+            Debug.Log("ALL VIEWED, LOADING END SCREEN!");   //debug log for testing
+            SceneManager.LoadScene("EndScreen");    // load end screen
         }
     }
 
